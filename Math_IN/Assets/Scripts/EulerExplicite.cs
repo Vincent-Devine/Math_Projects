cc1c532125dcf3ce1e64f5f3ceb52b0ef80937b4
Vincent DEVINE <vincentdevine.pro@gmail.com>
2024-09-17 10:51:26 +0200

    ADD euler explicite compute

--- Math_IN/Assets/Scripts/EulerExplicite.cs
+++ Math_IN/Assets/Scripts/EulerExplicite.cs
@@ -1,19 +1,42 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class EulerExplicite : MonoBehaviour
 {
-    public float[,] ComputeEulerExplicite(Func<float, float> func, float tmin, float tmax, float yO, float h)
+    Dictionary<float, float> curve = new Dictionary<float, float>();
+
+    private void Start()
     {
-        float t = tmin;
-        while(t >= tmax)
-        {
-
-            t += h;
-        }
-
-        return;
+        // exo 1
+        Func<float, float, float> func1 = (tn, yn) => Mathf.Exp(2 * tn) + yn;
+        func1 = (tn, yn) => yn;
+        ComputeEulerExplicite(func1, 0.0f, 5.0f, 1.0f, 5.0f / 1000.0f);
     }
 
-    private float Compute
+    private void Update()
+    {
+        for(int i = 0; i < curve.Count - 1; i++)
+        {
+            Vector3 start = new Vector3(0, curve.ElementAt(i).Value, curve.ElementAt(i).Key);
+            Vector3 end = new Vector3(0, curve.ElementAt(i + 1).Value, curve.ElementAt(i + 1).Key);
+            Debug.DrawLine(start, end, Color.blue);
+        }
+    }
+
+    public void ComputeEulerExplicite(Func<float, float, float> func, float tmin, float tmax, float yO, float h)
+    {
+        float t = tmin;
+        float yn = yO;
+        curve.Clear();
+
+        while (t <= tmax)
+        {
+            float yApproximate = yn + h * func(t, yn);
+            float nextY = yn + (h/2) * (func(t, yn) + func(t + h, yApproximate));
+            curve.Add(t, nextY);
+            t += h;
+        }
+    }
 }
